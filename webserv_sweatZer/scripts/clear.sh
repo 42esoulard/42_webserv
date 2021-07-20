6007f52d72c41c0c50d59901636e169fc6cf807b
esoulard <esoulard@student.42.fr>
2021-07-20 19:33:49 +0200

    precious

--- webserv_sweatZer/scripts/clear.sh
+++ webserv_sweatZer/scripts/clear.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Deleting all test generated files..."
+MAGENTA="\033[1;35m"
+NC="\033[0;0m"
+BOLD=$(tput bold)
+NB=$(tput sgr0)
+
+echo -e "\n${MAGENTA}${BOLD}Deleting all test generated files...${NB}${NC}"
 rm www/YoupiBanane/uploads/*
 rm www/sweatzer/uploads/*
 rm tests/acoudert_tester/pythonTest/putStuff/*
-echo "Good boi! Now won't u look at that webserv, all spick and span and bright as a new penny!"
+echo -e "${MAGENTA}${BOLD}Good boi! Now won't u look at that webserv, all spick and span and bright as a new penny!${NB}${NC}\n"
